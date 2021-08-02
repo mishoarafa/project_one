b261b7c569bdedc1560277d9c9ebf385f1d3e0ed
Mohamed Arafa <45900241+mishoarafa@users.noreply.github.com>
2021-08-02 23:47:57 +0200

    dio commit

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -6,13 +6,44 @@ class BusinessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.network(
-          "https://cdni.rt.com/media/pics/2021.07/original/6105808c4c59b722741256db.JPG",
+        Container(
           width: 120,
           height: 120,
-          fit: BoxFit.cover,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  "https://www.elmostaqbal.com/elmostaqbal/uploads/2021/07/www.elmostaqbal.com_2021-07-23_02-02-07_071108.jpg"),
+            ),
+          ),
         ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+
+          children: [
+            Text(
+              "Title",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              "2021-08-01T09:56:10Z",
+              style: TextStyle(
+                color: Colors.grey,
+
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
